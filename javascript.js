fbe868225338b3462413c970b80db34c74a80090
[Mateusz] <onny.mat@gmail.com>
2017-06-02 18:44:47 +0200

    Responsive menu

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        const id = this.hash.slice(1);
+        target = target.length ? target : $('[name=' + id +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, () => {
+            location.hash = id;
+          });
+          return false;
+        }
       }
-    }
-  });
+    });
+
+    $('.header__nav a').click(() => {    
+      $('.js-menu-checkbox').prop('checked', false);
+    })
