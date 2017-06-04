3d1f9788df0c04f343463776af0a6823e847ca37
[Mateusz] <onny.mat@gmail.com>
2017-06-04 10:56:57 +0200

    contact us section alhpa version

--- javascript.js
+++ javascript.js
@@ -17,3 +17,14 @@
     $('.header__nav a').click(() => {    
       $('.js-menu-checkbox').prop('checked', false);
     })
+ function initMap() {
+        var uluru = {lat: 54.361385, lng: 18.649726};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }    
